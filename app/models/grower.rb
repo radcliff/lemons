2bfff209154c6d7bcd0d869daa3b82326cd825b1
Sterling Radcliff <radcliff@riseup.net>
2015-07-16 00:59:26 -0700

    rails generate devise grower - add model: Grower w/ devise helpers

---
+++ app/models/grower.rb
@@ -0,0 +1,10 @@
+class Grower < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  validates :email, uniqueness: true
+  validates_presence_of :name, :zipcode
+
+end
