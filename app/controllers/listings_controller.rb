c29bc3c52453210846c249bfeea1e47b0dd3a56d
Sterling Radcliff <radcliff@riseup.net>
2015-07-25 13:40:43 -0700

    load kinds (types) dynamically from json resource

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,4 +1,16 @@
 class ListingsController < ApplicationController
   def new
+    @kinds = kinds
   end
+
+private
+  def kinds
+    response = Faraday.get ENV['ITEM_KINDS_URL']
+    if response.success?
+      JSON.parse(response.body, symbolize_names: true)[:kinds] unless not response.success?
+    else
+      [ { option: "Lemons", value: "lemons" } ]  # default options
+    end 
+  end
+
 end
